ec642ae268dcdbc241e917cf277e46b74b0beae7
AlyMBarakat <aly.m.barakat@gmail.com>
2021-01-04 05:12:51 +0300

    Change state to switchState

--- view/index.js
+++ view/index.js
@@ -6,7 +6,7 @@ const switchBtn = [
     document.getElementById("garage"),
 ];
 
-let state = [false, false, false, false];
+let switchState = [false, false, false, false];
 let roomsNames = [
     "Living Room",
     "Kitchen",
@@ -15,10 +15,10 @@ let roomsNames = [
 ]
 
 switchBtn[0].addEventListener('click', () => {
-    if (state[0])
-        state[0] = false;
+    if (switchState[0])
+        switchState[0] = false;
     else
-        state[0] = true;
+        switchState[0] = true;
 
     if (!window.Notification) return;
     try {
@@ -34,10 +34,10 @@ switchBtn[0].addEventListener('click', () => {
 });
 
 switchBtn[1].addEventListener('click', () => {
-    if (state[1])
-        state[1] = false;
+    if (switchState[1])
+        switchState[1] = false;
     else
-        state[1] = true;
+        switchState[1] = true;
 
     if (!window.Notification) return;
     try {
@@ -53,10 +53,10 @@ switchBtn[1].addEventListener('click', () => {
 });
 
 switchBtn[2].addEventListener('click', () => {
-    if (state[2])
-        state[2] = false;
+    if (switchState[2])
+        switchState[2] = false;
     else
-        state[2] = true;
+        switchState[2] = true;
 
     if (!window.Notification) return;
     try {
@@ -72,10 +72,10 @@ switchBtn[2].addEventListener('click', () => {
 });
 
 switchBtn[3].addEventListener('click', () => {
-    if (state[3])
-        state[3] = false;
+    if (switchState[3])
+        switchState[3] = false;
     else
-        state[3] = true;
+        switchState[3] = true;
 
     if (!window.Notification) return;
     try {
@@ -94,7 +94,7 @@ function showNotification(permission, roomNumber) {
     if (permission !== 'granted')
         return;
 
-    if (state[roomNumber]) {
+    if (switchState[roomNumber]) {
         let notification = new Notification('Smart Home app', {
             body: roomsNames[roomNumber] + " light is ON",
         });
