6c4857c1762abcdb50d034637cfed23531d24a3b
AlyMBarakat <aly.m.barakat@gmail.com>
2021-01-04 04:37:57 +0300

    Clean index.js

--- view/index.js
+++ view/index.js
@@ -3,19 +3,10 @@ const button = document.querySelector("input");
 button.addEventListener('click', () => {
 
     if (!window.Notification) return;
-
-    // Notification
-    //     .requestPermission()
-    //     .then(showNotification)
-
-
     try {
         Notification.requestPermission()
             .then(showNotification)
     } catch (error) {
-        // Safari doesn't return a promise for requestPermissions and it                                                                                                                                       
-        // throws a TypeError. It takes a callback as the first argument                                                                                                                                       
-        // instead.
         if (error instanceof TypeError) {
             Notification.requestPermission(showNotification);
         } else {
