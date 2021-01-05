9622092737e2fb6a686e2c5218fcdf4de633f0d7
AlyMBarakat <aly.m.barakat@gmail.com>
2021-01-05 06:43:10 +0300

    Refactor to Express application

---
+++ app.js
@@ -0,0 +1,21 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// route handler for the default home page
+app.get("/", (req, res) => {
+  res.send("iot-smart-home-broker V0.0.1");
+});
+
+
+module.exports = app;
